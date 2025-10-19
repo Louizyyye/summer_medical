e68da8d504a304ca6c23cfc05955b70af297f9c2
Dreteck <andredenmark005@gmail.com>
2025-10-19 12:52:42 +0300

    otp push comments

--- summermedical-backend/index.js
+++ summermedical-backend/index.js
@@ -135,4 +135,4 @@ app.get("/", (_, res) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`🚀 Server running on port ${PORT}`));
